a2b0dd6e1a9cb8a8cb930232c49c8266ab0f75d7
yxhj513 <944178627@qq.com>
2014-10-13 19:14:58 +0800

    "Add login controller"

--- ims_php_pure/application/views/www/page/login.php
+++ ims_php_pure/application/views/www/page/login.php
@@ -43,14 +43,19 @@
             <!--<form id="loginForm" action="logincontroller.php/LoginController/login" method="post" class="form-horizontal">-->
 
             <?php 
-            $attributes = array('class' => 'form-horizontal', 'id' => 'loginForm');
+            $attributes = array('class' => 'form-horizontal');
             echo form_open('logincontroller/login', $attributes); 
+            //echo form_open('logincontroller/login');
             ?>
                 <input type = "hidden" name = "submited" value = "true"/>
                 <div class="updateLine loginTitle text">
                     请首先登录后台信息管理系统...
+                    <?php
+                    if (empty($errorInformation) == FALSE) {
+                        echo $erroInformation;
+                    }
+                    ?>
                 </div>
-
                 <div class="updateLine">
                     <span class="text">邮箱：<span class="warning warningNickName" id="warningEmail">*</span></span>
                     <input type="text" name="username" id="loginEmail" class="form-control input" placeholder="输入邮箱" />
@@ -58,7 +63,7 @@
 
                 <div class="updateLine">
                     <span class="text">密码：<span class="warning old_password_msg input_msg" id="warningPassword">*</span></span>
-                    <input type="password" class="form-control old_password input" id="loginPassword" placeholder="输入密码" name='user.password'>
+                    <input type="password" class="form-control old_password input" id="loginPassword" placeholder="输入密码" name='user_password'>
                 </div>
 
                 <div class="updateLine" style="margin-top: 25px; ">
