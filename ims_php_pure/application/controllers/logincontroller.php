64c93ee13ef8f8ba1cfe2dc1cc78b3f372365759
yxhj513 <944178627@qq.com>
2014-10-13 19:09:08 +0800

    "Add login controller"

--- ims_php_pure/application/controllers/logincontroller.php
+++ ims_php_pure/application/controllers/logincontroller.php
@@ -12,11 +12,24 @@ class LoginController extends CI_Controller {
     
     public function login() {
         $username = $this->input->post('username');
+        $password = $this->input->post('user_password');
+
+        $this->load->model('usermodel');
         
-        if ($username != '123') {
-            $this->load->view('login');
-        } else {
-            $this->load->view('home');
+        $res = $this->usermodel->exists($username, $password);
+  
+        switch($res) {
+            case -1: // User doesn't exist
+                $data['errorInformation'] = 'User doesn\'t exist';
+                $this->load->view('login', $data);
+                break;
+            case 0: // Password is wrong
+                $data['errorInformation'] = 'Password is wrong';
+                $this->load->view('login', $data);
+                break;
+            case 1: // Both are correct
+                $this->load->view('home');
+                break;
         }
     }
 }
